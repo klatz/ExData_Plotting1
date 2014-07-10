5b118abbd4dece352d98b3c406578cf989279fc1
klatz <klatz.krt@gmail.com>
2014-07-11 03:38:11 +0900

    plot3 legend smaller
    let legend be smaller

--- plot3.R
+++ plot3.R
@@ -15,7 +15,8 @@ EPC$Date2 <- strptime(paste(EPC$Date,EPC$Time,sep="/"),"%d/%m/%Y/%H:%M:%S")
 plot(EPC$Date2,EPC$Sub_metering_1,type="l",xlab="",ylab="Energy sub metering")
 lines(EPC$Date2,EPC$Sub_metering_2,type="l",col="red")
 lines(EPC$Date2,EPC$Sub_metering_3,type="l",col="blue")
-legend("topright",lwd=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+legend("topright",lwd=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3")
+                ,cex=0.9,y.intersp=0.7)
 
 ## save as PNG file
 dev.copy(png,file="plot3.png")
